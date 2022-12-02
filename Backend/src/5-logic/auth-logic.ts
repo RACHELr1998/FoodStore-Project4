c04c589ca2e2b5d8610c03722a7d5d62b95d228f
RACHELr1998 <rachelyv1998@gmail.com>
2022-12-02 13:33:59 +0200

    update logic's files

---
+++ Backend/src/5-logic/auth-logic.ts
@@ -0,0 +1,78 @@
+import auth from "../2-utils/auth";
+import hash from "../2-utils/cyber";
+import { UnauthorizeError, ValidationError } from "../4-models/client-errors";
+import {
+  CredentialsModel,
+  ICredentialsModel,
+} from "../4-models/credentials-model";
+import { CustomerModel, ICustomerModel } from "../4-models/customer-model";
+
+async function register(customer: ICustomerModel): Promise<string> {
+  // Returning a new token
+  // Validate:
+  const error = customer.validateSync();
+  if (error) throw new ValidationError(error.message);
+
+  // hash password
+  customer.password = hash(customer.password);
+
+  // insert the new customer to the DB
+  customer.save();
+
+  // Delete customer password before return:
+  delete customer.password;
+
+  // Generate new token:
+  const token = auth.generateNewToken(customer);
+
+  return token;
+}
+
+async function login(credentials: ICredentialsModel) {
+  // :Promise<string>
+  // Validate:
+  const error = credentials.validateSync();
+  if (error) throw new ValidationError(error.message);
+
+  // hash password
+  credentials.password = hash(credentials.password);
+
+  // Get the customer by his credentials
+  const customer = CredentialsModel.findOne({
+    username: credentials.username,
+    password: credentials.password,
+  }).exec();
+
+  // If no such customer exists:
+  if (!customer) throw new UnauthorizeError("Incorrect username or password!");
+
+  // Delete customer password before return:
+  delete (await customer).password;
+
+  // Generate new token:
+  //   const token = auth.generateNewToken(customer);
+
+  //   return token;
+}
+
+async function usernameExists(username: string): Promise<boolean> {
+  // Get amount of customers with 'username'
+  const usernameExists = CustomerModel.findOne(
+    {
+      username: username,
+    },
+    ["username"]
+  ).exec();
+  // if there are more than 0, the username exists.
+  if (!usernameExists) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export default {
+  register,
+  login,
+  usernameExists,
+};
--- Backend/src/5-logic/product-logic.ts
+++ Backend/src/5-logic/product-logic.ts
@@ -4,34 +4,40 @@ import { IProductModel, ProductModel } from "../4-models/product-model";
 
 // Get all categories:
 function getAllCategories(): Promise<ICategoryModel[]> {
-    return CategoryModel.find().exec(); 
+  return CategoryModel.find().exec();
 }
 
 // Get all products by categoryId:
-function getAllProductsByCategoryId(categoryId: string): Promise<IProductModel[]> {
-    if (!categoryId) throw new IdNotFoundError(categoryId);
-    return ProductModel.find({categoryId}).populate("category").exec(); 
+function getAllProductsByCategoryId(
+  categoryId: string
+): Promise<IProductModel[]> {
+  if (!categoryId) throw new IdNotFoundError(categoryId);
+  return ProductModel.find({ categoryId }).populate("category").exec();
 }
 
 // Add product By Admin:
 function addProduct(product: IProductModel): Promise<IProductModel> {
-    const errors = product.validateSync();
-    if (errors) throw new ValidationError(errors.message);
-    return product.save(); 
+  const errors = product.validateSync();
+  if (errors) throw new ValidationError(errors.message);
+  return product.save();
 }
 
 // Update product by Admin:
 async function updateProduct(product: IProductModel): Promise<IProductModel> {
-    const errors = product.validateSync();
-    if (errors) throw new ValidationError(errors.message);
-    const updatedProduct = await ProductModel.findByIdAndUpdate(product._id, product, { returnOriginal: false }).exec(); // { returnOriginal: false } --> return back db product and not argument product.
-    if (!updatedProduct) throw new IdNotFoundError(product._id);
-    return updatedProduct;
+  const errors = product.validateSync();
+  if (errors) throw new ValidationError(errors.message);
+  const updatedProduct = await ProductModel.findByIdAndUpdate(
+    product._id,
+    product,
+    { returnOriginal: false }
+  ).exec(); // { returnOriginal: false } --> return back db product and not argument product.
+  if (!updatedProduct) throw new IdNotFoundError(product._id);
+  return updatedProduct;
 }
 
 export default {
-    getAllProductsByCategoryId,
-    getAllCategories,
-    addProduct,
-    updateProduct
+  getAllCategories,
+  getAllProductsByCategoryId,
+  addProduct,
+  updateProduct,
 };
