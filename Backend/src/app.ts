63258f3e1b7d9d563b523616f09b6cdd05e86b0a
RACHELr1998 <rachelyv1998@gmail.com>
2022-12-02 13:32:16 +0200

    update app.ts file

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,7 +5,7 @@ import express from "express";
 import cors from "cors";
 import catchAll from "./3-middleware/catch-all";
 import routeNotFound from "./3-middleware/route-not-found";
-import controller from "./6-controllers/controller";
+import controller from "./6-controllers/authController";
 import config from "./2-utils/config";
 
 const server = express();
@@ -16,4 +16,6 @@ server.use("/api", controller);
 server.use("*", routeNotFound);
 server.use(catchAll);
 
-server.listen(config.port, () => console.log("Listening on http://localhost:" + config.port));
+server.listen(config.port, () =>
+  console.log("Listening on http://localhost:" + config.port)
+);
