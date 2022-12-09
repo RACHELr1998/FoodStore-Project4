120e355d466f7dbc9f241e5916879df980eeb9df
RACHELr1998 <rachelyv1998@gmail.com>
2022-12-09 10:53:51 +0200

    update client-errors file

--- Backend/src/4-models/client-errors.ts
+++ Backend/src/4-models/client-errors.ts
@@ -1,38 +1,46 @@
+import { ObjectId } from "mongoose";
+
 export class ClientError {
-    public status: number;
-    public message: string;
-    public constructor(status: number, message: string) {
-        this.status = status;
-        this.message = message;
-    }
+  public status: number;
+  public message: string;
+  public constructor(status: number, message: string) {
+    this.status = status;
+    this.message = message;
+  }
 }
 
 export class IdNotFoundError extends ClientError {
-    public constructor(_id: string) {
-        super(404, `_id ${_id} not found`);
-    }
+  public constructor(_id: string) {
+    super(404, `_id ${_id} not found`);
+  }
+}
+
+export class ObjectIdNotFoundError extends ClientError {
+  public constructor(_id: ObjectId) {
+    super(404, `_id ${_id} not found`);
+  }
 }
 
 export class RouteNotFoundError extends ClientError {
-    public constructor(route: string) {
-        super(404, `route ${route} not found`);
-    }
+  public constructor(route: string) {
+    super(404, `route ${route} not found`);
+  }
 }
 
 export class ValidationError extends ClientError {
-    public constructor(message: string) {
-        super(400, message);
-    }
+  public constructor(message: string) {
+    super(400, message);
+  }
 }
 
 export class UnauthorizeError extends ClientError {
-    public constructor(message: string) {
-        super(401, message);
-    }
+  public constructor(message: string) {
+    super(401, message);
+  }
 }
 
 export class ForbiddenError extends ClientError {
-    public constructor(message: string) {
-        super(403, message);
-    }
+  public constructor(message: string) {
+    super(403, message);
+  }
 }
