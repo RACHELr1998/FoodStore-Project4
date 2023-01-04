ecacbcdfc76adee0aca3024cd78a4a1e1e3c9a4d
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-05 00:16:03 +0200

    update and create files of layout-area

--- Frontend/src/app/components/layout-area/header/header.component.ts
+++ Frontend/src/app/components/layout-area/header/header.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Unsubscribe } from "redux";
+import { CustomerModel } from "src/app/models/customer-model.model";
+import { authStore } from "src/app/redux/AuthState";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  public customer: CustomerModel;
+  private unsubscribe: Unsubscribe;
+  //   public pathByUserState: string;
 
-  constructor() { }
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
+    this.customer = authStore.getState().customer;
+    this.unsubscribe = authStore.subscribe(() => {
+      this.customer = authStore.getState().customer;
+
+      //   if (!this.customer) {
+      //     this.pathByUserState = "/home";
+      //   } else {
+      //     this.pathByUserState = "/shop";
+      //   }
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
 }
