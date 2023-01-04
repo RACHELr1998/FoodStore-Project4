86414fecb2bc25b0177bd32cf78df6e0b12a2294
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-05 00:13:12 +0200

    update routing-module file

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { CartComponent } from "./components/cart-area/cart/cart.component";
-import { AddDataComponent } from "./components/data-area/add-data/add-data.component";
-import { ListDataComponent } from "./components/data-area/list-data/list-data.component";
+import { LogoutComponent } from "./components/auth-area/logout/logout.component";
+import { RegisterBothStepsComponent } from "./components/auth-area/register/register-both-steps/register-both-steps.component";
+import { CartComponent } from "./components/shop-area/cart-area/cart/cart.component";
 import { HomeComponent } from "./components/home-area/home/home.component";
-import { EditProductComponent } from "./components/products-area/edit-product/edit-product.component";
+import { AddProductComponent } from "./components/admin-area/add-product/add-product.component";
+import { EditProductComponent } from "./components/admin-area/edit-product/edit-product.component";
+import { ProductsListComponent } from "./components/shop-area/products-area/products-list/products-list.component";
+import { PageNotFoundComponent } from "./components/layout-area/page-not-found/page-not-found.component";
 
 const routes: Routes = [
+  { path: "register", component: RegisterBothStepsComponent },
+  { path: "logout", component: LogoutComponent },
   { path: "home", component: HomeComponent },
+  { path: "shop", component: ProductsListComponent },
+  { path: "new", component: AddProductComponent },
   { path: "edit/:_id", component: EditProductComponent },
-  { path: "add", component: AddDataComponent },
   { path: "cart", component: CartComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  // { path: "**", component: PageNotFoundComponent },
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
