7112923f57a33a5c390bef6f01e3f6da83ab8628
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-05 00:13:26 +0200

    update module file

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { LayoutComponent } from './components/layout-area/layout/layout.component';
-import { HeaderComponent } from './components/layout-area/header/header.component';
-import { MenuComponent } from './components/layout-area/menu/menu.component';
-import { HomeComponent } from './components/home-area/home/home.component';
-import { AddDataComponent } from './components/data-area/add-data/add-data.component';
-import { ListDataComponent } from './components/data-area/list-data/list-data.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductCardComponent } from './components/products-area/product-card/product-card.component';
-import { AddProductComponent } from './components/products-area/add-product/add-product.component';
-import { EditProductComponent } from './components/products-area/edit-product/edit-product.component';
-import { CartComponent } from './components/cart-area/cart/cart.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { LayoutComponent } from "./components/layout-area/layout/layout.component";
+import { HeaderComponent } from "./components/layout-area/header/header.component";
+import { MenuComponent } from "./components/layout-area/menu/menu.component";
+import { HomeComponent } from "./components/home-area/home/home.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ProductCardComponent } from "./components/shop-area/products-area/product-card/product-card.component";
+import { AddProductComponent } from "./components/admin-area/add-product/add-product.component";
+import { EditProductComponent } from "./components/admin-area/edit-product/edit-product.component";
+import { CartComponent } from "./components/shop-area/cart-area/cart/cart.component";
+import { NgbDatepickerModule, NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ProductModalComponent } from "./components/shop-area/products-area/product-modal/product-modal.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ProductsListComponent } from "./components/shop-area/products-area/products-list/products-list.component";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatCardModule } from "@angular/material/card";
+import { LoginComponent } from "./components/auth-area/login/login.component";
+import { AuthMenuComponent } from "./components/auth-area/auth-menu/auth-menu.component";
+import { FooterComponent } from "./components/layout-area/footer/footer.component";
+import { JwtModule } from "@auth0/angular-jwt";
+import { LogoutComponent } from "./components/auth-area/logout/logout.component";
+import { Interceptor } from "./services/interceptor";
+import { RegisterStepOneComponent } from "./components/auth-area/register/register-step-one/register-step-one.component";
+import { RegisterStepTwoComponent } from "./components/auth-area/register/register-step-two/register-step-two.component";
+import { RegisterBothStepsComponent } from "./components/auth-area/register/register-both-steps/register-both-steps.component";
+import { MatStepperModule } from "@angular/material/stepper";
+import { CategoriesListComponent } from "./components/shop-area/products-area/categories-list/categories-list.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
+import { SearchProductsComponent } from "./components/shop-area/products-area/search-products/search-products.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { PageNotFoundComponent } from "./components/layout-area/page-not-found/page-not-found.component";
+import { CartItemComponent } from "./components/shop-area/cart-area/cart-item/cart-item.component";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { CartItemDeleteComponent } from "./components/shop-area/cart-area/cart-item-delete/cart-item-delete.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { OrderComponent } from "./components/shop-area/order-area/order/order.component";
+import { AddOrderComponent } from "./components/shop-area/order-area/add-order/add-order.component";
+import { EndOrderMessageComponent } from "./components/shop-area/order-area/end-order-message/end-order-message.component";
+import { InvoicePDFComponent } from "./components/shop-area/order-area/invoice-pdf/invoice-pdf.component";
+import { MatSelectModule } from "@angular/material/select";
+import { MatDatepickerModule } from "@angular/material/datepicker";
 
 @NgModule({
   declarations: [
@@ -20,20 +51,58 @@ import { CartComponent } from './components/cart-area/cart/cart.component';
     HeaderComponent,
     MenuComponent,
     HomeComponent,
-    AddDataComponent,
-    ListDataComponent,
     ProductCardComponent,
-    AddProductComponent,
-    EditProductComponent,
-    CartComponent
+    // AddProductComponent,
+    // EditProductComponent,
+    CartComponent,
+    ProductModalComponent,
+    ProductsListComponent,
+    LoginComponent,
+    AuthMenuComponent,
+    FooterComponent,
+    LogoutComponent,
+    RegisterStepOneComponent,
+    RegisterStepTwoComponent,
+    RegisterBothStepsComponent,
+    CategoriesListComponent,
+    SearchProductsComponent,
+    PageNotFoundComponent,
+    CartItemComponent,
+    CartItemDeleteComponent,
+    OrderComponent,
+    AddOrderComponent,
+    EndOrderMessageComponent,
+    InvoicePDFComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
+    NgbDatepickerModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    JwtModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatListModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatDatepickerModule,
   ],
-  providers: [],
-  bootstrap: [LayoutComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Interceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [LayoutComponent],
 })
-export class AppModule { }
+export class AppModule {}
