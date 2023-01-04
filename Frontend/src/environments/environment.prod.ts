2971223cce0b247a340f782e9858b5d40b6b8c59
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-05 00:22:21 +0200

    update environment files

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
   production: true,
   productsUrl: "http://localhost:3001/api/products/",
+  categoriesUrl: "http://localhost:3001/api/categories/",
+  imagesUrl: "http://localhost:3001/api/images/",
   cartUrl: "http://localhost:3001/api/cart/",
+  customerCartUrl: "http://localhost:3001/api/cart-by-customer/",
   orderUrl: "http://localhost:3001/api/order/",
+  authUrl: "http://localhost:3001/api/auth/",
 };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -5,8 +5,12 @@
 export const environment = {
   production: false,
   productsUrl: "http://localhost:3001/api/products/",
+  categoriesUrl: "http://localhost:3001/api/categories/",
+  imagesUrl: "http://localhost:3001/api/images/",
   cartUrl: "http://localhost:3001/api/cart/",
+  customerCartUrl: "http://localhost:3001/api/cart-by-customer/",
   orderUrl: "http://localhost:3001/api/order/",
+  authUrl: "http://localhost:3001/api/auth/",
 };
 
 /*
