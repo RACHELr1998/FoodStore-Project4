70b0c84e796eff8e0339927f017e4f37757b21b5
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-05 00:10:29 +0200

    create order files

---
+++ Backend/src/5-logic/orders-logic.ts
@@ -0,0 +1,33 @@
+import hash from "../2-utils/cyber";
+import { ValidationError } from "../4-models/client-errors";
+import { IOrderModel, OrderModel } from "../4-models/order-model";
+
+
+//Adding a new order:
+async function addOrder(order: IOrderModel): Promise<IOrderModel> {
+    const errors = order.validateSync();
+    if (errors) throw new ValidationError(errors.message);
+
+    order.creditCard = hash(order.creditCard);
+
+    // await cartLogic.closeCart(order.cartId.toString());
+
+    //Add an order:
+    return order.save();
+};
+
+//Get all orders:
+async function getAllOrders(): Promise<IOrderModel[]> {
+    return OrderModel.find().populate("cart").populate("user").exec();
+};
+
+//Count the number of orders:
+async function countOrders(): Promise<number> {
+    return OrderModel.find().count().exec();
+};
+
+export default {
+    addOrder,
+    getAllOrders,
+    countOrders
+}
---
+++ Backend/src/6-controllers/orders-controller.ts
@@ -0,0 +1,39 @@
+import express, { NextFunction, Request, Response } from "express";
+import verifyLoggedIn from "../3-middleware/verify-logged-in";
+import { OrderModel } from "../4-models/order-model";
+import ordersLogic from "../5-logic/orders-logic";
+
+const router = express.Router();
+
+// GET http://localhost:3001/api/orders/
+router.get("/orders", verifyLoggedIn, async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const orders = await ordersLogic.getAllOrders();
+        response.json(orders);
+    } catch (err: any) {
+        next(err);
+    }
+});
+
+// POST http://localhost:3001/api/orders/
+router.post("/orders", verifyLoggedIn, async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const order = new OrderModel(request.body);
+        const addedOrder = await ordersLogic.addOrder(order);
+        response.status(201).json(addedOrder);
+    } catch (err: any) {
+        next(err);
+    }
+});
+
+// GET http://localhost:3001/api/orders/count
+router.get("/orders/count", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const count = await ordersLogic.countOrders();
+        response.json(count);
+    } catch (err: any) {
+        next(err);
+    }
+});
+
+export default router;
