9c773f38fc560fc609297b7920a94a12bf44a10b
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-08 16:33:04 +0200

    update app-routing file

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -8,15 +8,34 @@ import { EditProductComponent } from "./components/admin-area/edit-product/edit-
 import { PageNotFoundComponent } from "./components/layout-area/page-not-found/page-not-found.component";
 import { MainShoppingPageComponent } from "./components/shop-area/main-shopping-page/main-shopping-page.component";
 import { OrderComponent } from "./components/shop-area/order-area/order/order.component";
+import { AdminHomeComponent } from "./components/admin-area/admin-home/admin-home.component";
+import { UseVerifyAdmin } from "./services/useVerifyAdmin";
+import { UseVerifyLoggedIn } from "./services/useVerifyLoggedIn";
 
 const routes: Routes = [
   { path: "register", component: RegisterBothStepsComponent },
   { path: "logout", component: LogoutComponent },
   { path: "home", component: HomeComponent },
-  { path: "shop", component: MainShoppingPageComponent },
-  { path: "order", component: OrderComponent },
-  { path: "new", component: AddProductComponent },
-  { path: "edit/:_id", component: EditProductComponent },
+  {
+    path: "shop",
+    component: MainShoppingPageComponent,
+    canActivate: [UseVerifyLoggedIn],
+  },
+  {
+    path: "order",
+    component: OrderComponent,
+    canActivate: [UseVerifyLoggedIn],
+  },
+  {
+    path: "admin-home",
+    component: AdminHomeComponent,
+    canActivate: [UseVerifyAdmin],
+  },
+  {
+    path: "admin/edit/:_id",
+    component: EditProductComponent,
+    canActivate: [UseVerifyAdmin],
+  },
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent },
 ];
