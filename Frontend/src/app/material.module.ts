9cc8cedbddc7d56204ae9fee0fa71c9995792688
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-08 16:33:39 +0200

    create material module file

---
+++ Frontend/src/app/material.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule, MAT_DATE_LOCALE } from "@angular/material/core";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+
+const materialComponents = [
+  MatIconModule,
+  MatButtonModule,
+  MatListModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatStepperModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatDialogModule,
+
+  MatSlideToggleModule,
+  //   MatCardModule,
+  //   MatStepperModule,
+  //   MatTabsModule,
+  //   MatIconModule,
+  //   MatFormFieldModule,
+  MatDividerModule,
+  //   MatListModule,
+  //   MatDialogModule,
+  //   MatSelectModule,
+  //   MatDatepickerModule,
+  //   MatSidenavModule,
+  //   MatNativeDateModule,
+  //   MatInputModule,
+];
+
+@NgModule({
+  imports: [materialComponents],
+  exports: [materialComponents],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: "en-IL" }],
+})
+export class MaterialModule {}
