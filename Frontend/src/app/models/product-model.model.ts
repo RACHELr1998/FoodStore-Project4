3c349a6945a8a052dfa4557397a82dff09677148
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-08 16:42:09 +0200

    update product-model

--- Frontend/src/app/models/product-model.model.ts
+++ Frontend/src/app/models/product-model.model.ts
@@ -2,9 +2,9 @@ import { CategoryModel } from "./category-model.model";
 
 export class ProductModel {
   public _id: string;
-  public productName: string;
   public categoryId: string;
   public categoryName: CategoryModel;
+  public productName: string;
   public price: number;
   public image: File;
   public imageName: string;
