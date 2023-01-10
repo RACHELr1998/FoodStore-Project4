a2de70de27ec9ce99664095b7de8d481b52c341b
RACHELr1998 <rachelyv1998@gmail.com>
2023-01-10 21:07:37 +0200

    edit app.ts file

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,17 +5,19 @@ import express from "express";
 import cors from "cors";
 import catchAll from "./3-middleware/catch-all";
 import routeNotFound from "./3-middleware/route-not-found";
-import authController from "./6-controllers/authController";
-import cartController from "./6-controllers/cartController";
-import productController from "./6-controllers/productController";
+import authController from "./6-controllers/auth-Controller";
+import cartController from "./6-controllers/cart-Controller";
+import productController from "./6-controllers/products-Controller";
 import orderController from "./6-controllers/orders-controller";
 import imagesController from "./6-controllers/images-controller";
 import config from "./2-utils/config";
+import expressFileUpload from "express-fileupload";
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+server.use(expressFileUpload());
 server.use("/api", authController);
 server.use("/api", cartController);
 server.use("/api", productController);
